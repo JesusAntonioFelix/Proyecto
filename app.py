0d5b1f3261a954033896429705a85a5c3392a178
Jesus Antonio Felix <j.antoniofelix98@gmail.com>
2024-04-11 18:36:51 -0700

    initial commit

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ import streamlit as st
 import plotly.express as px
 
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+st.header('Graficos venta de carros') 
 hist_button = st.button('Construir histograma') # crear un botón
 
-st.header('Graficos')                                                                   
+                                                                   
 if hist_button: # al hacer clic en el botón
             # escribir un mensaje
  st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
@@ -15,11 +16,16 @@ if hist_button: # al hacer clic en el botón
         
         # mostrar un gráfico Plotly interactivo
  st.plotly_chart(fig, use_container_width=True)                                         
-st.write(
+
+hist_button2 = st.button('Construir diagrama de dispersion') # crear un botón
+
+if hist_button2:# al hacer clic en el botón
+            # escribir un mensaje
+    st.write(
         'Creación de un diagrama de dispersion para el conjunto de datos de anuncios de venta de coches')
 
     # crear un gráfico de dispersión
-fig2 = px.scatter(car_data, x="odometer", y="price")
+    fig2 = px.scatter(car_data, x="odometer", y="price")
 
 # mostrar un gráfico Plotly interactivo
-st.plotly_chart(fig2, use_container_width=True)           
+    st.plotly_chart(fig2, use_container_width=True)           
